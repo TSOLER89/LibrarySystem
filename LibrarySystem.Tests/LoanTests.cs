aa98cbe30e898cfc4bbad488551cf483520903ab
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-06 14:25:36 +0100

    RED: Loan overdue test created

--- LibrarySystem.Tests/LoanTests.cs
+++ LibrarySystem.Tests/LoanTests.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Xunit;
+using LibrarySystem.Core.Models;
 
 namespace LibrarySystem.Tests
 {
-    internal class LoanTests
+    public class LoanTests
     {
+        [Fact]
+        public void Loan_Should_Be_Overdue_After_14_Days()
+        {
+            // Arrange
+            var book = new Book ("Test", "Author", "1")
+            var member = new Member("Alice", 1);
+            
+            var loan = new LoanTests (book, member, DateTime.Now.AddDays(-15));
+            //Assert
+            Assert.True(loan.IsOverdue);
+
+        }
     }
 }
