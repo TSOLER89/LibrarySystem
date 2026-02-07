f0d743667fc1dd14de69a516495efb51c06fb14f
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-07 08:18:51 +0100

    RED: Library statistics test for total books

--- LibrarySystem.Tests/LibraryStatisticsTests.cs
+++ LibrarySystem.Tests/LibraryStatisticsTests.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Xunit;
+using LibrarySystem.Core.Services;
 using System.Collections.Generic;
-using System.Text;
+
 
 namespace LibrarySystem.Tests
 {
-    internal class LibraryStatisticsTests
+    public class LibraryStatisticsTests
     {
+        [Fact]
+
+        public void Should_Return_Total_Number_Of_Books()
+        {
+            // Arrange
+            var books = new List<Book>
+            {
+                new Book("A", "A", "1"),
+                new Book("B", "B", "2"),
+            };
+            var library = new Library(books, new List<Loan>());
+
+            Assert.Equal(2, library.TotalBooks());
+        }
     }
+
 }
