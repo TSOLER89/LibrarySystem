63e619969f595b75b095fd2edfb0ebce75ab95e0
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-08 19:25:06 +0100

    Add unit test for sorting books by title in catalog
    "RED: Sorting books by title test"

---
+++ LibrarySystem.Tests/SortingTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using System.Text;
+using LibrarySystem.Core.Models;
+
+namespace LibrarySystem.Tests;
+
+    public class SortingTests
+{
+    [Fact]
+    public void Should_Sort_Books_bY_Title()
+    {
+        var books = new List<Book>
+        {
+                new Book("C", "A", "1", "2000"),
+                new Book("B", "A", "2", "2001"),
+                new Book("A", "B", "3", "2002")
+        };
+                   var catalog = new BookCatalog(books);
+
+                    var sorted = catalog.SortByTitle();
+
+                    Assert.Equal("A", sorted[0].Title);
+                    Assert.Equal("B", sorted[1].Title);
+                    Assert.Equal("C", sorted[2].Title);
+
+
+
+                }
+            }
+        }
+
+    }
+}
+
