a3ef508c3e2002cfab1efd7ad56f2fc01b41cc89
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-08 19:03:04 +0100

    Add unit test for Book.Matches and update SearchTests class
    "RED: Search test for Book"

--- LibrarySystem.Tests/SearchTests.cs
+++ LibrarySystem.Tests/SearchTests.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Xunit;
 using System.Collections.Generic;
 using System.Text;
+using LibrarySystem.Core.Models;
 
 namespace LibrarySystem.Tests
 {
-    internal class SearchTests
+    public class SearchTests
     {
+        [Fact]
+        public void Book_Should_Match_Search_By_Title_Or_Author()
+        {
+            var book = new Book("1984", "George Orwell", "1");
+
+            Assert.True(book.Matches("1984"));
+            Assert.True(book.Matches("George Orwell"));
+            Assert.False(book.Matches("Brave New World"));
+
+        }
     }
 }
