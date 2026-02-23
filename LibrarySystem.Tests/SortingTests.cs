2e1ecddfe4d6800414deeb8282e51f8af7de060a
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-23 10:11:18 +0100

    Update tests for int publication year; remove ISBN test

--- LibrarySystem.Tests/SortingTests.cs
+++ LibrarySystem.Tests/SortingTests.cs
@@ -1,31 +1,29 @@
 ﻿using Xunit;
 using System.Collections.Generic;
-using System;
+using System.Text;
 using LibrarySystem.Core.Models;
 using LibrarySystem.Core.Services;
 
-
-namespace LibrarySystem.Tests;
-
-public class SortingTests
+namespace LibrarySystem.Tests
 {
-    [Fact]
-    public void Should_Sort_Books_bY_Title()
+    public class SortingTests
     {
-        var books = new List<Book>
+        [Fact]
+        public void Should_Sort_Books_bY_Title()
         {
-                new Book("1", "C", "A", "2000"),
-                new Book("2", "B", "A", "2001"),
-                new Book("3", "A", "B", "2002")
-        };
-        var catalog = new BookCatalog(books);
+            var books = new List<Book>
+            {
+                new Book("1", "C", "A", 2000),
+                new Book("2", "B", "A", 2001),
+                new Book("3", "A", "B", 2002)
+            };
+            var catalog = new BookCatalog(books);
 
-        var sorted = catalog.SortByTitle();
+            var sorted = catalog.SortByTitle();
 
-        Assert.Equal("A", sorted[0].Title);
-        Assert.Equal("B", sorted[1].Title);
-        Assert.Equal("C", sorted[2].Title);
+            Assert.Equal("A", sorted[0].Title);
+            Assert.Equal("B", sorted[1].Title);
+            Assert.Equal("C", sorted[2].Title);
+        }
     }
 }
-
-        
