496e88224b3cd64ea9be13fcff4e87523dd0f8bc
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-23 10:09:36 +0100

    Update test for new domain model constructors & dependencies

--- LibrarySystem.Tests/LibraryBorrowedBooksTests.cs
+++ LibrarySystem.Tests/LibraryBorrowedBooksTests.cs
@@ -1,31 +1,33 @@
 ﻿using Xunit;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using LibrarySystem.Core.Services;
 using LibrarySystem.Core.Models;
+using LibrarySystem.Core.Services;
 
 namespace LibrarySystem.Tests
 {
     public class LibraryBorrowedBooksTests
     {
         [Fact]
-        // Test method to verify the number of borrowed books
         public void Should_Return_Number_Of_Borrowed_Books()
         {
-            // Arrange
-            var book1 = new Book("1", "Book1", "Author", "2024");
-            var book2 = new Book("2", "Book2", "Author", "2024");
+            var book1 = new Book("1", "Book1", "Author", 2024);
+            var book2 = new Book("2", "Book2", "Author", 2024);
 
-            var member = new Member(1, "Alice");
+            var member = new Member(1, "Alice", "alice@test.com", DateTime.Now);
 
             var loan = new Loan(book1, member, DateTime.Now.AddDays(-5));
 
             var books = new List<Book> { book1, book2 };
             var loans = new List<Loan> { loan };
 
-            var library = new Library(books, loans);
+            var bookCatalog = new BookCatalog(books);
+            var memberRegistry = new MemberRegistry();
+            var loanManager = new LoanManager(loans);
+            var library = new Library(bookCatalog, memberRegistry, loanManager);
 
-            // Act
-            Assert.Equal(1, library.BorrowedBooksCount());        }
+            Assert.Equal(1, library.BorrowedBooksCount());
+        }
     }
 }
