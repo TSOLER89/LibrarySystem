aea0b5537c803157388054b2e645ee63828e0172
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-23 10:07:03 +0100

    Update test to use new Library constructor and Book API
    Refactored Should_Return_Total_Number_Of_Books test to use
    BookCatalog, MemberRegistry, and LoanManager with the updated
    Library constructor. Changed Book publication year from string
    to integer. Removed obsolete comments and blank lines.

--- LibrarySystem.Tests/LibraryStatisticsTests.cs
+++ LibrarySystem.Tests/LibraryStatisticsTests.cs
@@ -1,31 +1,29 @@
 ﻿using Xunit;
 using LibrarySystem.Core.Services;
 using LibrarySystem.Core.Models;
+using System;
 using System.Collections.Generic;
-
 
 namespace LibrarySystem.Tests
 {
     public class LibraryStatisticsTests
     {
         [Fact]
-        // Test to verify that the TotalBooks method returns the correct count of books in the library
-
         public void Should_Return_Total_Number_Of_Books()
         {
             // Arrange
             var books = new List<Book>
             {
-                // Creating two book instances to add to the library
-                new Book("1", "A", "A", "2024"),
-                new Book("2", "B", "B", "2024"),
+                new Book("1", "A", "A", 2024),
+                new Book("2", "B", "B", 2024),
             };
-            // Creating a library instance with the list of books and an empty list of loans
-            var library = new Library(books, new List<Loan>());
 
-            // Act
+            var bookCatalog = new BookCatalog(books);
+            var memberRegistry = new MemberRegistry();
+            var loanManager = new LoanManager();
+            var library = new Library(bookCatalog, memberRegistry, loanManager);
+
             Assert.Equal(2, library.TotalBooks());
         }
     }
-
 }
