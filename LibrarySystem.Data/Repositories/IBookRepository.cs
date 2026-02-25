0ad9ee4884e8a10adaf29f46dd00cbfd0bf49cc3
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-25 17:36:44 +0100

    Add IBookRepository interface and SQLite db files
    Added IBookRepository.cs with async CRUD methods for Book entity, including search functionality.

---
+++ LibrarySystem.Data/Repositories/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using LibrarySystem.Core.Models;
+
+namespace LibrarySystem.Data.Repositories;
+
+public interface IBookRepository
+{
+    // CRUD operations for Book entity //
+    // Task betyder “asynkront” (körs utan att låsa appen)
+    Task<IEnumerable<Book>> GetAllAsync();
+
+    //Book? betyder “kan vara null” om boken inte finns
+    Task<Book?> GetByIdAsync(int id);
+    Task<Book?> GetByISBNAsync(string isbn);
+    Task AddAsync(Book book);
+    Task UpdateAsync(Book book);
+    Task DeleteAsync(int id);
+    Task<IEnumerable<Book>> SearchAsync(string searchTerm);
+}
