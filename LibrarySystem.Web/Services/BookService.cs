52d95dbb375e5d7146ea8811c761229cae28a23c
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-26 10:38:57 +0100

    Introduced BookService in LibrarySystem.Web.Services to encapsulate IBookRepository. Provides async methods for CRUD and search operations, supporting dependency injection and improved separation of concerns.

---
+++ LibrarySystem.Web/Services/BookService.cs
@@ -0,0 +1,26 @@
+﻿using LibrarySystem.Core.Models;
+using LibrarySystem.Data.Repository;
+
+namespace LibrarySystem.Web.Services;
+
+public class BookService
+{
+    private readonly IBookRepository _repo;
+
+    public BookService(IBookRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public Task<IEnumerable<Book>> GetAllAsync() => _repo.GetAllAsync();
+
+    public Task<IEnumerable<Book>> SearchAsync(string term) => _repo.SearchAsync(term);
+
+    public Task<Book?> GetByIdAsync(int id) => _repo.GetByIdAsync(id);
+
+    public Task AddAsync(Book book) => _repo.AddAsync(book);
+
+    public Task UpdateAsync(Book book) => _repo.UpdateAsync(book);
+
+    public Task DeleteAsync(int id) => _repo.DeleteAsync(id);
+}
